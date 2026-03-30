c383c7d3325862dca2bc38a774941ea724c7e207
miray1214268 <s1214268@student.windesheim.nl>
2026-03-31 01:09:51 +0200

    spiffs partitie aangemaakt en roofvogel.wav toegevoegd

--- src/main.c
+++ src/main.c
@@ -1,1 +1,31 @@
-void app_main() {}
+#include <stdio.h>
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_spiffs.h"
+
+static const char *TAG = "example";
+
+void spiffs_init() {
+esp_vfs_spiffs_conf_t conf = {
+.base_path = "/spiffs",
+.partition_label = NULL,
+.max_files = 2,
+.format_if_mount_failed = true
+};
+ESP_ERROR_CHECK(esp_vfs_spiffs_register(&conf));
+}
+
+void app_main() {
+FILE *file = fopen("/roofvogel.wav", "rb");
+if (!file) {
+ESP_LOGE(TAG, "Kan bestand niet openen: %s",
+"/spiffs/roofvogel.wav");
+return;
+} else {
+ESP_LOGI(TAG, "bestand geopend: %s",
+"/spiffs/roofvogel.wav");
+}
+}
